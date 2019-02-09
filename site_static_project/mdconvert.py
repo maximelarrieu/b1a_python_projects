7cd7316a9709ec902eecc54ecc427588a23c5e71
maximelarrieu <maxime.larrieu@ynov.com>
2019-02-09 17:07:01 +0100

    test avec boucle for, list, toujours pas de conversion par dossier

--- site_static_project/mdconvert.py
+++ site_static_project/mdconvert.py
@@ -3,33 +3,37 @@
 import argparse
 import markdown
 import codecs
-import glob
+import re
 import os
 
 def to_Html(markdown_directory, html_directory):
-    counter = 0
-    md_file = codecs.open(markdown_directory, "r")
-    body = md_file.read()
-    html = markdown.markdown(body)
-    html_file = open(html_directory + 'index{counter}', "w")
-    html_file.write(html)
-    html_file.close()
-    counter += 1
+    #number_of_file = 0
+    index = 0
+    for md_files in md_files_list:
+        print(md_files_list[1])
+        md_file = codecs.open(md_files_list[index], 'r', 'utf-8')
+        body = md_file.read()
+        html = markdown.markdown(body)
+        html_file = open(html_directory.html, 'w')
+        html_file.write(html)
+        html_file.close()
+        index += 1
 
 parser = argparse.ArgumentParser()
 input_directory = '--input-directory'
 output_directory = '--output-directory'
 template_directory = '--template-directory'
-markdown_directory = 'markedown_files/'
-md_files = os.listdir(markdown_directory)
-html_directory = 'html_files/'
-#html_files = 'index2.html'
 
-parser.add_argument("-i", input_directory, help="sources files folder (contains markdown files)", action="store_true")
-parser.add_argument("-o", output_directory, help="final folder where files will be generated", action="store_true")
+markdown_directory = 'markdown_files/'
+md_files_list = os.listdir(markdown_directory)
+html_directory = 'html_files/'
+
+parser.add_argument("-i", input_directory, "--input", help="sources files folder (contains markdown files)", action="store_true")
+parser.add_argument("-o", output_directory, "--output", help="final folder where files will be generated", action="store_true")
 parser.add_argument("-t", template_directory, help="folder containing web models to complete", action="store_true")
 args = parser.parse_args()
-args.input = args.input_directory
-args.ouput = args.output_directory
 
-to_Html(markdown_directory, html_directory)
+markdown_directory = args.input_directory
+html_directory = args.output_directory
+
+to_Html(args.input_directory, args.output_directory)
